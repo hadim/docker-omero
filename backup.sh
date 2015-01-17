09aff26d2e8f76ee497b83eb4c815bf2bdf24e64
Hadrien Mary <hadrien.mary@gmail.com>
2015-01-17 18:03:17 +0100

    Don't use compression for backup system (only tar)

--- backup.sh
+++ backup.sh
@@ -7,6 +7,6 @@ else
     BACKUP_DIR=$HOME/omero_backup
 fi
 
-BACKUP_NAME=$(date +%Y_%m_%d_%H_%M_omero.tar.bz2)
+BACKUP_NAME=$(date +%Y_%m_%d_%H_%M_omero.tar)
 
-docker run --rm=true --volumes-from data -v $BACKUP_DIR:/tmp ubuntu tar -jcvf /tmp/$BACKUP_NAME /data
+docker run --rm=true --volumes-from data -v $BACKUP_DIR:/tmp ubuntu tar -cvf /tmp/$BACKUP_NAME /data
--- restore.sh
+++ restore.sh
@@ -4,11 +4,11 @@ if [ ! -z $1 ]
 then
     BACKUP_FILE=$1
 else
-    echo "You must specify a backup file (.tar.bz2) to restore."
+    echo "You must specify a backup file (.tar) to restore."
     exit 1
 fi
 
 BACKUP_DIR=$(dirname $BACKUP_FILE)
 BACKUP_NAME=$(basename $BACKUP_FILE)
 
-docker run --name data omero-data true && docker run --rm=true --volumes-from data -v $BACKUP_DIR:/tmp ubuntu tar xjvf /tmp/$BACKUP_NAME
+docker run --name data omero-data true && docker run --rm=true --volumes-from data -v $BACKUP_DIR:/tmp ubuntu tar xvf /tmp/$BACKUP_NAME
